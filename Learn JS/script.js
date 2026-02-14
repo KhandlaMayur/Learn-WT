eb0dd13fcdad31d99ec578112f463809eb407356
KhandlaMayur <khandlamayur62@gmail.com>
2026-02-15 00:03:50 +0530

    update the introduction the javascript

---
+++ Learn JS/script.js
@@ -0,0 +1,18 @@
+alert("Hello Mayur");
+
+console.log("Mayur")
+console.log("Khandla")
+
+var a = prompt("Enter a number")
+console.log("Your number is " +a)
+var isTrue =confirm("Are You Sure you want to learn javascript?")
+
+if(isTrue){
+    console.log("lets learn javascript")
+}
+else{
+    console.log("Okay, maybe later")
+}
+
+document.title = "Welcome to JavaScript"
+document.body.style.backgroundColor = "lightblue"
