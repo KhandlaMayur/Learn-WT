f3d98e951ca4964ac6fca6f4bf37dbe31aee686c
KhandlaMayur <khandlamayur62@gmail.com>
2026-02-16 15:57:06 +0530

    upload the loop tutorial which i learn

---
+++ Learn JS/loops/loop.js
@@ -0,0 +1,39 @@
+console.log("Hello I am to learn 4th tutorial of JavaScript Loops")
+
+// let a = 1
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i)
+    
+// }
+
+// let object ={
+//     name : "Mayur",
+//     Age : 22, 
+//     City : "Surendranagar"
+// }
+
+// for (const key in object) {
+    
+//     // const element = object[key];
+//     console.log(key +":",object[key])
+    
+    
+// }
+
+// for (const i of "Mayur") {
+//     console.log(i)
+// }
+
+
+// let a = 1
+// while(a < 100){
+//     console.log(a)
+//     a++
+// }
+
+let a =1
+do {
+    console.log(a)
+    a++
+    
+} while (a < 10);
