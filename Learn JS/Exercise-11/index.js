f6d6728a66a04df13482404ebfce98a550253ed4
KhandlaMayur <khandlamayur62@gmail.com>
2026-02-18 18:15:48 +0530

    find the factorial of n number using reduce method in this exercise

--- Learn JS/Exercise-11/index.js
+++ Learn JS/Exercise-11/index.js
@@ -1,6 +1,24 @@
-let n = prompt("Enter the number") 
-let sum = 1
-for (let i = 1; i <=n; i++) {
-    sum =sum * i
+// Using For Loop
+
+
+// let n = prompt("Enter the number") 
+// let sum = 1
+// for (let i = 1; i <=n; i++) {
+//     sum =sum * i
+// }
+// console.log(sum)
+
+
+// Using Reduce Method 
+
+let a = 6
+function factorial(number){
+    let arr=Array.from(Array(number+1).keys())  // from  = array start with, number+1 = because my array start from 1 , keys() == is the function to return all element up to n
+    console.log(arr.slice(1,))
+    let c = arr.slice(1,).reduce((a,b) => {
+        return a*b
+    })
+    console.log(c)
 }
-console.log(sum)
+
+factorial(a)
